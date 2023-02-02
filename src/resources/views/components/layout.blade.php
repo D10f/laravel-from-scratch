696e4223fb5a0cb11d022abf50174bad33cd6088
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-02-02 13:05:27 +0100

    Installed TailwindCSS, adjusted templates and removed unneeded views

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel From Scratch Blog</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 
 <body style="font-family: Open Sans, sans-serif">
