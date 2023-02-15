c0def366a591fb313d7f6b2125780cbe515f8c1d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-02-15 18:13:14 +0100

    Add form to submit new comments

--- src/resources/views/posts/show.blade.php
+++ src/resources/views/posts/show.blade.php
@@ -52,6 +52,24 @@
         </div>
 
         <section class="col-span-8 col-start-5 mt-10">
+
+            <form action="" class="bg-gray-100 p-6 mb-4 rounded-xl border border-gray-200">
+                @csrf
+
+                <header class="flex items-center">
+                    <img class="rounded-xl" src="https://i.pravatar.cc/100" alt="User avatar" width="60px" height="60px">
+                    <h2 class="ml-4">Want to participate?</h2>
+                </header>
+
+                <div class="mt-6">
+                    <textarea class="w-full text-sm p-2 bg-gray-100 focus:outline-none focus:ring" name="comment" id="comment" placeholder="Quick, think of something to say..." cols="30" rows="5" ></textarea>
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold py-2 px-10 text-xs rounded-2xl hover:bg-blue-600">Submit</button>
+                </div>
+            </form>
+
             @foreach ($post->comments as $comment)
                 <x-post-comment :comment="$comment" />
             @endforeach
